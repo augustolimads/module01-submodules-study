243da0a6718202a4bd31c4615c73d4f8ad16694f
Augusto Lima <augustolimads@gmail.com>
2021-11-03 16:09:19 -0300

    add styled components

--- src/components/CounterButtons.jsx
+++ src/components/CounterButtons.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 import style from "./style.module.css";
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 12px;
+`;
+
+const Button = styled.button`
+  flex: 1;
+  color: black;
+  font-size: 20px;
+  background-color: blue;
+  border: none;
+  border-radius: 4px;
+  &:active {
+    background-color: black;
+    color: white;
+  }
+`;
 
 export function CounterButtons({ increase, decrease }) {
   return (
-    <div className={style.buttons}>
-      <button className={style.btn} onClick={increase}>
-        +
-      </button>
-      <button className={style.btn} onClick={decrease}>
-        -
-      </button>
-    </div>
+    <Buttons>
+      <Button onClick={increase}>+</Button>
+      <Button onClick={decrease}>-</Button>
+    </Buttons>
   );
 }
